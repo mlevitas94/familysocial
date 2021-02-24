954c22fe870e7ec2d85c27059399f34b6fcbcbe8
mlevitas94 <levitasmc@gmail.com>
2021-02-24 13:01:56 -0500

    dashboard routes added

--- components/logged/Dashboard.js
+++ components/logged/Dashboard.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Groups } from './loggedComponents/Groups';
+import { VChats } from './loggedComponents/VChats';
+
+const Tab = createBottomTabNavigator();
 
 export const Dashboard = () => {
     return (
-        <View>
-            <Text>hi</Text>
-        </View>
+        <Tab.Navigator>
+            <Tab.Screen name="Groups" component={ Groups} />
+            <Tab.Screen name="VChats" component={ VChats} />
+        </Tab.Navigator>
     );
 }
-
-const styles = StyleSheet.create();
---
+++ components/logged/loggedComponents/Groups.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+
+export const Groups = () => {
+    return (
+        <View>
+            <Text>hi groups</Text>
+        </View>
+    );
+}
