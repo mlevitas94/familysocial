7b54c23cced556559f5a90db3d8c6f7ca4859fab
mlevitas94 <levitasmc@gmail.com>
2021-02-26 14:41:43 -0500

    style variables added

--- components/logged/loggedComponents/Groups.js
+++ components/logged/loggedComponents/Groups.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import variables from '../../../styleLists/variables'
 
 
 
@@ -12,8 +13,8 @@ export const Groups = () => {
             <Text style={{ textAlign: 'center', marginTop: 44, marginBottom: 44 }}>You are not in any groups.</Text>
             <TouchableOpacity style={{ width: 120 }}>
                 <View style={{ alignItems: 'center', flexDirection: 'row', justifyContent: 'center' }}>
-                    <FontAwesomeIcon icon={faPlusCircle} style={{ marginRight: 5 }} />
-                    <Text>Create Group</Text>
+                    <FontAwesomeIcon icon={faPlusCircle} style={{ marginRight: 5 , color:variables.buttonColor1}} />
+                    <Text style={{color:variables.buttonColor1}}>Create Group</Text>
                 </View>
             </TouchableOpacity>
 
---
+++ styleLists/variables.js
@@ -0,0 +1,3 @@
+export default {
+    buttonColor1 : '#007AFF'
+}
