6611eb084600354c953f1d2c3af997960e9b7b33
mlevitas94 <levitasmc@gmail.com>
2021-02-26 14:36:43 -0500

    create group fixed touchableOpacity

--- components/logged/Dashboard.js
+++ components/logged/Dashboard.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Groups } from './loggedComponents/Groups';
 import { VChats } from './loggedComponents/VChats';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faUsers, faVideo } from '@fortawesome/free-solid-svg-icons'
 
 const Tab = createBottomTabNavigator();
 
 export const Dashboard = () => {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Groups" component={ Groups} />
-            <Tab.Screen name="VChats" component={ VChats} />
+            <Tab.Screen name="Groups" component={ Groups} options={{
+                tabBarIcon:() => <FontAwesomeIcon icon={faUsers}/>
+            }} />
+            <Tab.Screen name="VChats" component={ VChats} options={{
+                tabBarIcon:() => <FontAwesomeIcon icon={faVideo}/>
+            }}/>
         </Tab.Navigator>
     );
 }
--- components/logged/loggedComponents/Groups.js
+++ components/logged/loggedComponents/Groups.js
@@ -1,12 +1,26 @@
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 
 export const Groups = () => {
     return (
-        <View>
-            <Text>hi groups</Text>
+        <View style={{ alignItems: 'center' }}>
+            <Text style={{ textAlign: 'center', marginTop: 44, marginBottom: 44 }}>You are not in any groups.</Text>
+            <TouchableOpacity style={{ width: 120 }}>
+                <View style={{ alignItems: 'center', flexDirection: 'row', justifyContent: 'center' }}>
+                    <FontAwesomeIcon icon={faPlusCircle} style={{ marginRight: 5 }} />
+                    <Text>Create Group</Text>
+                </View>
+            </TouchableOpacity>
+
+
         </View>
     );
 }
+const styles = StyleSheet.create({
+
+})
